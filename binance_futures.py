825b3ca63d72f9bc05aa9630cc734fcf9351f276
vindurthi <51331769+vindurthi@users.noreply.github.com>
2021-12-13 18:29:41 -0800

    added function to get contracts for all symbols

--- binance_futures.py
+++ binance_futures.py
@@ -1,5 +1,6 @@
 import logging
-import main
+import requests
+import pprint
 
 logger = logging.getLogger()
 
@@ -7,3 +8,19 @@ logger = logging.getLogger()
 "https://testnet.binancefuture.com"
 
 "wss://fstream.binance.com"
+
+
+def get_contracts():
+
+    response_object = requests.get("https://fapi.binance.com/fapi/v1/exchangeInfo")
+    print(response_object.status_code ) 
+
+    contracts = []
+
+    
+    for contract in response_object.json()['symbols']:        
+        contracts.append(contract['pair'])
+
+    return contracts
+
+print(get_contracts())
